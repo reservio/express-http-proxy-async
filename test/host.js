6007a04d5917282415cf588212ce7ab1cc0c88f8
Oron Nadiv <oron@nadiv.us>
2017-11-01 14:29:14 -0700

    re-writing parts of the unit tests using Nock.

--- test/host.js
+++ test/host.js
@@ -3,32 +3,32 @@
 var express = require('express');
 var request = require('supertest');
 var proxy = require('../');
+var Chance = require('chance');
+var chance = new Chance();
+var nock = require('nock');
 
 describe('host can be a dynamic function', function() {
 
   this.timeout(10000);
 
-  var app = express();
   describe('and memoization can be disabled', function() {
-      var firstProxyApp = express();
-      var secondProxyApp = express();
-      // TODO: This seems like a bug factory.  We will have intermittent port conflicts, yeah?
       var firstPort = Math.floor(Math.random() * 65536);
       var secondPort = Math.floor(Math.random() * 65536);
+      var domain = chance.domain();
+      nock('http://' + domain + ':' + firstPort)
+        .get('/')
+        .reply(204);
+      nock('http://' + domain + ':' + secondPort)
+        .get('/')
+        .reply(200);
 
       var hostFn = function(req) {
-        return 'localhost:' + req.params.port;
+        return 'http://' + domain + ':' + req.params.port;
       };
 
+      var app = express();
+
       app.use('/proxy/:port', proxy(hostFn, { memoizeHost: false }));
-
-      firstProxyApp
-          .get('/', function(req, res) { res.sendStatus(204); })
-          .listen(firstPort);
-
-      secondProxyApp
-          .get('/', function(req, res) { res.sendStatus(200); })
-          .listen(secondPort);
 
       it('when not memoized, host resolves to a second value on the seecond call', function(done) {
         request(app)
@@ -51,18 +51,18 @@ describe('host is a promise', function() {
   this.timeout(10000);
 
   var app = express();
-  var proxyApp = express();
-  var port = Math.floor(Math.random() * 10000);
+
+  var port = Math.floor(Math.random() * 65536);
+  var domain = chance.domain();
+  nock('http://' + domain + ':' + port)
+    .get('/')
+    .reply(201);
 
   var hostFn = function(req) {
-    return Promise.resolve('localhost:' + req.params.port);
+    return 'http://' + domain + ':' + req.params.port;
   };
 
   app.use('/proxy-promise/:port', proxy(hostFn, { memoizeHost: false }));
-
-  proxyApp
-    .get('/', function(req, res) { res.sendStatus(201); })
-    .listen(port);
 
   it('host returns a promise that should be resolved and its value is the host URL.', function(done) {
     request(app)
