85af365a3de36ea6c6bb6c529906286c3b8495c3
Radoslav Šupolík <radosupolik@gmail.com>
2019-10-02 08:10:47 +0200

    support set response for decorateProxyReqOpt - now it's possible to set cookie for response

--- app/steps/decorateProxyReqOpts.js
+++ app/steps/decorateProxyReqOpts.js
@@ -5,14 +5,15 @@ function defaultDecorator(proxyReqOptBuilder /*, userReq */) {
 }
 
 function decorateProxyReqOpt(container) {
-  var resolverFn = container.options.proxyReqOptDecorator || defaultDecorator;
+    var resolverFn = container.options.proxyReqOptDecorator || defaultDecorator;
 
-  return Promise
-    .resolve(resolverFn(container.proxy.reqBuilder, container.user.req))
-    .then(function(processedReqOpts) {
-        delete processedReqOpts.params;
-        container.proxy.reqBuilder = processedReqOpts;
-        return Promise.resolve(container);
+    return Promise
+        .resolve(resolverFn(container.proxy.reqBuilder, container.user.req, container.user.res))
+        .then(function(data) {
+            delete data.proxyReq.params;
+            container.proxy.reqBuilder = data.proxyReq;
+            container.user.res = data.res;
+            return Promise.resolve(container);
     });
 }
 
